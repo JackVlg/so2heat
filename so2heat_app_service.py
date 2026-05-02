cb32c248999a0804756e72f584ae09e1b3d295c1
e.vologzhanin <jackvlgv@gmail.com>
2026-05-02 20:40:03 +0300

    new files

--- so2heat_app_service.py
+++ so2heat_app_service.py
@@ -1,3 +1,24 @@
-import so2heat_motor
+import requests
+import time
+from requests.exceptions import ConnectTimeout, ConnectionError
 
-print("Hello")
+import so2heat_motor
+from so2heat_request import SO2HeatRequest
+
+timeout = 5
+
+def makeRequest() :
+    result = SO2HeatRequest()
+
+while True:
+    print("Next time")
+
+    request = makeRequest();
+
+    try:
+        response = requests.post('https://jackvlg.ru:8878/api/v1/', json=request, timeout=(4, 3))
+    except ConnectTimeout as e:
+        print("Connection timed out")
+
+    time.sleep(timeout)
+
---
+++ so2heat_request.py
@@ -0,0 +1,6 @@
+class SO2HeatRequest() :
+
+    def __init__(self, photo):
+        self.photo = photo
+
+
