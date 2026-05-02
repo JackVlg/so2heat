1c5401cf7459817e198393f2b59485ceb9eac31d
e.vologzhanin <jackvlgv@gmail.com>
2026-05-02 20:40:53 +0300

    fixed url

--- so2heat_app_service.py
+++ so2heat_app_service.py
@@ -16,7 +16,7 @@ while True:
     request = makeRequest();
 
     try:
-        response = requests.post('https://jackvlg.ru:8878/api/v1/', json=request, timeout=(4, 3))
+        response = requests.post('https://test.ru:8878/api/v1/', json=request, timeout=(4, 3))
     except ConnectTimeout as e:
         print("Connection timed out")
 
