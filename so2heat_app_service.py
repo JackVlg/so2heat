800d05f04d888aee07946f197773b0c669822a88
e.vologzhanin <jackvlgv@gmail.com>
2026-05-03 13:01:50 +0300

    fixed url

--- so2heat_app_service.py
+++ so2heat_app_service.py
@@ -1,22 +1,30 @@
 import requests
 import time
+import random
+import json
+import base64
+import os
 from requests.exceptions import ConnectTimeout, ConnectionError
 
 import so2heat_motor
-from so2heat_request import SO2HeatRequest
+from so2heat_request import SO2HeatRequest, RequestEncoder
 
 timeout = 5
 
 def makeRequest() :
-    result = SO2HeatRequest()
+    test_binary_array = bytearray("Hello", "utf-8")
+    result = json.dumps(SO2HeatRequest(test_binary_array), indent=4, cls=RequestEncoder)
+    print("result: ", result)
+    return result
+
 
 while True:
     print("Next time")
 
-    request = makeRequest();
+    request = makeRequest()
 
     try:
-        response = requests.post('https://test.ru:8878/api/v1/', json=request, timeout=(4, 3))
+        response = requests.post('https://127.0.0.2:8878/api/v1/what-we-will-', json=request, timeout=(4, 3))
     except ConnectTimeout as e:
         print("Connection timed out")
 
