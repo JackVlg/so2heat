7979039bc9c885672596d585d4d18733b4e60946
e.vologzhanin <jackvlgv@gmail.com>
2026-08-16 16:27:20 +0300

    Начал делать модуль камеры

---
+++ village/so2heat_camera.py
@@ -0,0 +1,33 @@
+from picamera2 import Picamera2
+
+class SO2HeatCamera():
+
+    camera : Picamera2
+
+    def __init__(self):
+        self.camera = Picamera2()
+        print("Camera created")
+
+        camera_config = self.camera.create_preview_configuration()
+        self.camera.configure(camera_config)
+        print("Camera configured")
+
+        self.camera.start()
+        print("Camera started")
+
+    def capture(self):
+        img = self.camera.capture_array()
+        return img
+
+    def stop(self):
+        self.camera.stop()
+        print("Camera stopped")
+
+if __name__ == "__main__":
+    camera = SO2HeatCamera()
+    try:
+        photo = camera.capture()
+        print(type(photo))
+    finally:
+        camera.stop()
+    print("Module test finished")
