d2612ebe11c2f032af68a24bc66a3424ac7f1d06
e.vologzhanin <jackvlgv@gmail.com>
2026-08-19 20:27:40 +0300

    Добавил работу мотора

--- common/so2heat_types.py
+++ common/so2heat_types.py
@@ -1,8 +1,11 @@
+from enum import StrEnum
+
+
 class ParameterKeys:
     ROTATE_SPEED = "rotateSpeed"
     ROTATE_DURATION = "rotateDuration"
 
-class CommandType:
+class CommandType(StrEnum):
     ROTATE = "ROTATE"
     PRESS_MAIN_BUTTON = "PRESS_MAIN_BUTTON"
 
